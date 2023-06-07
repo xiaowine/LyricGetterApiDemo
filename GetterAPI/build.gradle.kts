cb9294e4e17946d8e41aad9a2bf306c32ebff5aa
xiaowine <xiaowine0@gmail.com>
2023-06-07 21:18:42 +0800

    fix

--- GetterAPI/build.gradle.kts
+++ GetterAPI/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    id("com.android.application")
+    id("kotlin-android")
 }
 
 android {
@@ -11,7 +11,6 @@ android {
     defaultConfig {
         minSdk = 26
 
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
